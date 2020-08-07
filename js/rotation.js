a96086d0a896a6feff0675bdd1377f4acd0601c9
jeancigoli <jean@primi.com.br>
2020-08-06 21:25:31 -0300

    New version

--- js/rotation.js
+++ js/rotation.js
@@ -1,29 +1,29 @@
-const $botaoGira = document.querySelector("#gira");
-const $SkillsFrente = document.querySelector("#caixa_skills");
-const $SkillsTras = document.querySelector("#caixa_skills_back");
+// const $botaoGira = document.querySelector("#gira");
+// const $SkillsFrente = document.querySelector("#caixa_skills");
+// const $SkillsTras = document.querySelector("#caixa_skills_back");
 
-let i = 0;
+// let i = 0;
 
-$botaoGira.addEventListener("click", () => {
+// $botaoGira.addEventListener("click", () => {
 
-    if(i == 0){
-        $SkillsFrente.style.transform = "rotateX(-90deg)";
-        $SkillsFrente.style.transition = "all 1s";
-        $SkillsTras.style.transform = "rotateX(0deg)";
-        $SkillsTras.style.transition = "all 1s";
+//     if(i == 0){
+//         $SkillsFrente.style.transform = "rotateX(-90deg)";
+//         $SkillsFrente.style.transition = "all 1s";
+//         $SkillsTras.style.transform = "rotateX(0deg)";
+//         $SkillsTras.style.transition = "all 1s";
 
-        $botaoGira.innerHTML = "Voltar &#10008;"
+//         $botaoGira.innerHTML = "Voltar &#10008;"
 
-        i = 1
-    } else if(i = 1){
-        $SkillsFrente.style.transform = "rotateX(0deg)";
-        $SkillsFrente.style.transition = "all 1s";
-        $SkillsTras.style.transform = "rotateX(90deg)";
-        $SkillsTras.style.transition = "all 1s";
+//         i = 1
+//     } else if(i = 1){
+//         $SkillsFrente.style.transform = "rotateX(0deg)";
+//         $SkillsFrente.style.transition = "all 1s";
+//         $SkillsTras.style.transform = "rotateX(90deg)";
+//         $SkillsTras.style.transition = "all 1s";
 
-        $botaoGira.innerHTML = "Veja mais &#10132;"
+//         $botaoGira.innerHTML = "Veja mais &#10132;"
 
-        i = 0;
-    }
+//         i = 0;
+//     }
 
-})
+// })
--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,31 @@
 $(document).ready(function(){
+
+	const menus = document.querySelectorAll('.menu_itens');
+
 	$(window).scroll(function(){ // função para pegar o movimento do scroll (barra de rolagem)
-	  var top = $(window).scrollTop(); // aqui vc pega a posição da página
+		var top = $(window).scrollTop();
+
+		/* Criação do efeito de selecionar o menu */
+		if(top >= 700 && top <= 1299) {
+			$(menus[0]).addClass('active');
+			$(menus[1]).removeClass('active');
+		} else if (top >= 1300 && top <= 1999) {
+			$(menus[1]).addClass('active');
+			$(menus[0]).removeClass('active');
+			$(menus[2]).removeClass('active');
+		} else if (top >= 2000 && top <= 2599) {
+			$(menus[2]).addClass('active');
+			$(menus[1]).removeClass('active');
+			$(menus[3]).removeClass('active');
+		} else if (top >= 2600) {
+			$(menus[3]).addClass('active');
+			$(menus[2]).removeClass('active');
+		} else {
+			$(menus[0]).removeClass('active');
+			$(menus[1]).removeClass('active');
+			$(menus[2]).removeClass('active');
+			$(menus[3]).removeClass('active');
+		}
 
 		if(top > 0){ 
 			$("#caixa_nave").addClass("menu_bar");
@@ -13,6 +38,7 @@ $(document).ready(function(){
 		}else{
 			$("#seta").hide();
 		}
+
 
 		if(top > 800){ 
 			$(".caixa_formacao").addClass("acao"); 
